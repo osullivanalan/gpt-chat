2c3bf339511f7adff51d0e250cc9c5060fb28b8b
osullivanalan <alan.osullivan@gmail.com>
2024-07-10 11:46:58 +0100

    vite config base home page github

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './gpt-chat' 
-});
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: './',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/gpt-chat/'
+  }
+
+  return config
+})
